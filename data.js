25b46969c86c3e72cf3dad1160c09ce5ace16eff
KennethMinn <minthukha2472005@gmail.com>
2024-04-18 14:08:30 +0630

    Adding table

---
+++ data.js
@@ -0,0 +1,32 @@
+export const m_data = [
+  {
+    id: 1,
+    name: "mtk",
+    email: "mtk@gmail.com",
+    gender: "male",
+  },
+  {
+    id: 2,
+    name: "st",
+    email: "st@gmail.com",
+    gender: "male",
+  },
+  {
+    id: 3,
+    name: "yma",
+    email: "yma@gmail.com",
+    gender: "male",
+  },
+  {
+    id: 4,
+    name: "nyi",
+    email: "nyi@gmail.com",
+    gender: "male",
+  },
+  {
+    id: 5,
+    name: "mama",
+    email: "mama@gmail.com",
+    gender: "female",
+  },
+];
---
+++ src/components/BasicTable.jsx
@@ -0,0 +1,72 @@
+import {
+  useReactTable,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
+import { m_data } from "../../data.js";
+import { useMemo } from "react";
+
+const BasicTable = () => {
+  //memoized data
+  const data = useMemo(() => m_data, []);
+
+  /** @type import('@tanstack/react-table').ColumnDef<any>*/
+  const columns = [
+    {
+      header: "ID",
+      accessorKey: "id",
+    },
+    {
+      header: "Name",
+      accessorKey: "name",
+    },
+    {
+      header: "Email",
+      accessorKey: "email",
+    },
+    {
+      header: "Gender",
+      accessorKey: "gender",
+    },
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <th key={header.id}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BasicTable;
